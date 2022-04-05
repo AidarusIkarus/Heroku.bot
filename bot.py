9977a1ce317f9af779edaa457f3a4a1ce9b62eb5
Aidar Muginov <muginov.aidar@gmail.com>
2022-04-05 20:00:59 +0300

    6

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import telebot
 bot = telebot.TeleBot('5201970475:AAHj_B25VK40N-3gKzdVt9rJs8FnRn6mDk8')
 @bot.message_handler(commands=['start'])
 def info(message):
-    bot.send_message(message.chat.id, 'Это мой бот!')
+    bot.send_message(message.chat.id, 'Илья крутой чел')
 
 bot.polling(none_stop=True)
 
