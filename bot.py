5fef2703198e1bf0436d4290ae703ead693c6216
Aidar Muginov <muginov.aidar@gmail.com>
2022-04-19 19:40:22 +0300

    7

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ keyboard.row('/start', '/info')
 keyboard.row('/change_mode')
 
 bot = telebot.TeleBot('5201970475:AAHj_B25VK40N-3gKzdVt9rJs8FnRn6mDk8')
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Вас приветствует бот проекта "Робот-тьютор"! Здесь вы можете выбрать режим игры:')
