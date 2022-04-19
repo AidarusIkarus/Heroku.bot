2e10734dec8833fc279b41f4848f1a2a7478fb84
Aidar Muginov <muginov.aidar@gmail.com>
2022-04-19 19:13:06 +0300

    7

--- bot.py
+++ bot.py
@@ -1,9 +1,23 @@
 import telebot
+import config
+from telebot.types import ReplyKeyboardMarkup
+from telebot.types import InlineKeyboardButton
+from telebot.types import InlineKeyboardMarkup
+keyboard = ReplyKeyboardMarkup(True)
+keyboard.row('/start', '/info')
+keyboard.row('/change_mode')
 
 bot = telebot.TeleBot('5201970475:AAHj_B25VK40N-3gKzdVt9rJs8FnRn6mDk8')
+@bot.message_handler(commands=['info'])
+def info_func (message):
+    inline=InlineKeyboardMarkup()
+
 @bot.message_handler(commands=['start'])
-def info(message):
-    bot.send_message(message.chat.id, 'Илья крутой чел')
+def start(message):
+    bot.send_message(message.chat.id, 'Вас приветствует бот проекта "Робот-тьютор"! Здесь вы можете выбрать режим игры:')
+@bot.message_handler(commands=['change_mod'])
+def change_mod(message):
+    bot.send_message(message.chat.id, 'lorem')
 
 bot.polling(none_stop=True)
 
