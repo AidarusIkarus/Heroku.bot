27323192b2b19ef63f1c88d5c737ef79f3cf344e
Aidar Muginov <muginov.aidar@gmail.com>
2022-04-26 18:38:45 +0300

    9

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('5201970475:AAHj_B25VK40N-3gKzdVt9rJs8FnRn6mDk8')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Вас приветствует Ассистент проекта "Робот-тьютор"!")
+    bot.send_message(message.chat.id, "Вас приветствует Ассистент проекта Робот-тьютор!")
 
 bot.polling(none_stop=True)
 
