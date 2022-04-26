9f2bc8eec10dc5c5d8927478bacc3a25d1c49df2
Aidar Muginov <muginov.aidar@gmail.com>
2022-04-26 18:54:08 +0300

    10

--- bot.py
+++ bot.py
@@ -8,6 +8,11 @@ bot = telebot.TeleBot('5201970475:AAHj_B25VK40N-3gKzdVt9rJs8FnRn6mDk8')
 def start(message):
     bot.send_message(message.chat.id, "Вас приветствует Ассистент проекта Робот-тьютор!")
 
+def button(message):
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn=types.KeyboardButton("Кнопка")
+    bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+  markup.add(btn)
 bot.polling(none_stop=True)
 
 
