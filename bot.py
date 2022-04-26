0494c482b53289c8d98e905edc04619a20e52cf2
Aidar Muginov <muginov.aidar@gmail.com>
2022-04-26 18:30:15 +0300

    9

--- bot.py
+++ bot.py
@@ -6,18 +6,8 @@ bot = telebot.TeleBot('5201970475:AAHj_B25VK40N-3gKzdVt9rJs8FnRn6mDk8')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id, 'Вас приветствует бот проекта "Робот-тьютор"! Здесь вы можете выбрать режим игры:')
+    bot.send_message(message.chat.id, "Вас приветствует Ассистент проекта "Робот-тьютор"!")
 
-@bot.message_handler(content_types=['text'])
-def func(message):
-    if(message.text == "👋 Поздороваться"):
-        bot.send_message(message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
-    elif(message.text == "❓ Задать вопрос"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 bot.polling(none_stop=True)
 
 
