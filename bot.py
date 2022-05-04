651e5366be6e64be8a601cb7fb2ce331dedc87c3
Aidar Muginov <muginov.aidar@gmail.com>
2022-05-04 19:01:51 +0300

    15

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot('5201970475:AAHj_B25VK40N-3gKzdVt9rJs8FnRn6mDk8')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Вас приветствует илья Ассистент проекта Робот-тьютор! Выберите что вам надо:')
+    bot.send_message(message.chat.id, 'Вас приветствует илья Ассистент проекта Робот-тьютор! Выберите что вам надо: ')
     inline=ReplyKeyboardMarkup()
     b1 = InlineKeyboardButton('Выбрать режим игры', callback_data='button1')
     inline.add(b1)
